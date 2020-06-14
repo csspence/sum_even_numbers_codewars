11d5f90014b05a275e1ae213e882e80b9e55d28e
Crew Spence <csspence123@gmail.com>
2020-06-14 07:52:19 -0400

    Wrote working solution.

--- sum_even_numbers_codewars.js
+++ sum_even_numbers_codewars.js
@@ -1,13 +1,22 @@
 /*
-Write a function named sumEvenNumbers, taking a sequence of numbers as single parameter. Your function must return the sum of the even values of this sequence.
+Write a function named sumEvenNumbers, taking a sequence of numbers as single parameter. 
+Your function must return the sum of the even values of this sequence.
 
 Only numbers without decimals like 4 or 4.0 can be even.
 
 Input
 sequence of numbers: those numbers could be integers and/or floats.
-For example, considering this input value : [4,3,1,2,5,10,6,7,9,8], then your function should return 30 (because 4 + 2 + 10 + 6 + 8 = 30).
+For example, considering this input value : [4,3,1,2,5,10,6,7,9,8], then your function should return 30 
+(because 4 + 2 + 10 + 6 + 8 = 30).
 */
 
-function sumEvenNumbers(input) {
-  // [...]
+const sumEvenNumbers = (input) => {
+  let sum = 0;
+  for(let i = 0; i < input.length; i++) {
+    if(input[i] % 2 === 0) {
+      sum += input[i];
+    }
+  }
+
+  return sum;
 }
